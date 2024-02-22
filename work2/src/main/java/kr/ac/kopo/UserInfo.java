6ad020e92a6463bf04bc5792e98938242c10ab8b
kdy37 <kdy37@DESKTOP-2QK37CB>
2024-02-22 23:27:13 +0900

    제조사명, 회원목록 인터페이스 구현

---
+++ work2/src/main/java/kr/ac/kopo/UserInfo.java
@@ -0,0 +1,33 @@
+package kr.ac.kopo;
+
+public class UserInfo {
+	private String name;
+	private String code;
+	private String date;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public UserInfo(String name, String code, String date) {
+		super();
+		this.name = name;
+		this.code = code;
+		this.date = date;
+	}
+	
+	
+}
